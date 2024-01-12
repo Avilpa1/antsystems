d029d046f3c4b34e4091a95563b7a2d81d0ed426
Paul <avila_pa@yahoo.com>
2024-01-11 20:08:56 -0800

    wip

--- antsystems-frontend/src/app/apps/shared/components/select/select.component.ts
+++ antsystems-frontend/src/app/apps/shared/components/select/select.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ComponentType } from '@angular/cdk/portal';
+import { AfterContentInit, ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { MdbModalService, MdbModalRef } from 'mdb-angular-ui-kit/modal';
 
 @Component({
   selector: 'ant-select',
@@ -7,7 +9,7 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./select.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SelectComponent {
+export class SelectComponent implements AfterContentInit {
   @Input() data!: any;
   @Input() groupName!: FormGroup;
   @Input() controlName: any;
@@ -19,11 +21,60 @@ export class SelectComponent {
   @Input() placeholder = 'Please Select'
   @Input() field: string = 'name';
   @Input() label: string | null = null;
+  @Input() valueChangedAlert: boolean = false;
+
   @Output() selectChangedEvent = new EventEmitter();
   
-  constructor() {}
+  prevValue: any;
+  modalRef: MdbModalRef<any> | null = null;
+  modalConfig = {
+    animation: true,
+    backdrop: true,
+    containerClass: 'right',
+    data: {
+      title: 'Warning!',
+      prev: '',
+      new: ''
+    },
+    ignoreBackdropClick: false,
+    modalClass: 'modal-dialog-centered',
+  }
+
+  constructor(private modalService: MdbModalService) {}
+
+  ngAfterContentInit(): void {
+    this.prevValue = this.controlName.value;
+  }
 
   selectChanged(event: Event) {
+    console.warn(this.prevValue, this.controlName.value)
+    if (this.valueChangedAlert && this.prevValue != null && this.prevValue !== this.controlName.value) {
+      if(this.controlName.value) {
+        this.modalConfig.data.prev = this.prevValue;
+        this.modalConfig.data.new = this.controlName.value;
+        this.modalRef = this.modalService.open(SelectValueChangedComponent, this.modalConfig);
+        this.modalRef.onClose.subscribe((value: any) => {
+          console.warn(value)
+          if (value) {
+            // Confirm
+            this.setValue(event)
+          } else {
+            // Cancel
+            this.controlName.setValue(this.prevValue, {emitEvent: false, onlySelf: true})
+          }
+        });        
+      } else {
+        // this.prevValue = null;
+        this.resetChildren();
+      }
+    } else {
+      this.prevValue = this.controlName.value;
+      this.setValue(event);
+    }
+  }
+
+  setValue(event: Event) {
+    console.warn('fired setValue')
     this.selectChangedEvent.next(event);
     this.resetChildren();
   }
@@ -37,4 +88,24 @@ export class SelectComponent {
     return c1 && c2 &&( c1.id === c2.id || c2 === c1.id);
  }
 
+}
+
+@Component({
+  selector: 'app-value-changed',
+  templateUrl: './select-value-changed.component.html',
+})
+export class SelectValueChangedComponent {
+  title: string | null = null;
+  prev: any | null = null;
+  new: any | null = null;
+  
+  constructor(public modalRef: MdbModalRef<SelectValueChangedComponent>) {}
+  
+  close(value: any): void {
+    this.modalRef.close(value)
+  }
+
+  confirm(value: any): void {
+    this.modalRef.close(value)
+  }
 }
