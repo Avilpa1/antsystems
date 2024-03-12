48e9753ace244e98ca3cb3c22ea47ba31b79bf73
Paul <avila_pa@yahoo.com>
2024-03-11 17:51:27 -0700

    update

--- antsystems-frontend/src/environments/environment.prod.ts
+++ antsystems-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://courageous-plum-hippopotamus.cyclic.app/'
+    apiUrl: 'https://courageous-plum-hippopotamus.cyclic.app'
 };
