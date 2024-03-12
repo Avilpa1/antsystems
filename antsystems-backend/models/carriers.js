e5febc73e9a8076dfa7149653ef8eaf0e4e00b59
Paul <avila_pa@yahoo.com>
2024-03-12 14:12:45 -0700

    update to carriers model

--- antsystems-backend/models/carriers.js
+++ antsystems-backend/models/carriers.js
@@ -20,6 +20,13 @@ const carrierSchema = new mongoose.Schema({
     phone: {
       type: String
     },
+    address: {
+      street: { type: String},
+      street2: { type: String},
+      city: { type: String},
+      state: { type: String},
+      zip: { type: String},
+    },
   }, { timestamps: true })
 
 module.exports = mongoose.model('carrier', carrierSchema)
